a2bd359bc270283d28257a4bfca87c878fceca77
Luis E.Velasquez <luiseduardo@Luiss-MacBook-Pro.local>
2020-06-15 17:42:26 +0300

    task 1 variables

--- story.js
+++ story.js
@@ -1,7 +1,13 @@
 // Define the name here on line 2
-
+let name = "Luis Eduardo";
 // Define the age here on line 4
-
+let age = 27;
 // Define the hobby here on line 6
-
+let hobby = "Reading";
 // Print the story here on line 8
+
+console.log(
+  `Mr./Mrs. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
